67075a3d0f5f29097a2b031f0b3c3b82b997e295
Nikolaos <95080307+Nikolaos-Gkionis@users.noreply.github.com>
2024-02-07 16:08:38 +0000

    Addin Hard Password
    - create a .env file to hide password
    - add middleware for proxy
    - include basic auth

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,25 +1,32 @@
+require('dotenv').config();
+const express = require('express');
+const basicAuth = require('express-basic-auth');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const server = express();
+
+server.use(basicAuth({
+  users: { [process.env.BASIC_AUTH_USER]: process.env.BASIC_AUTH_PASSWORD },
+  challenge: true,
+  realm: 'Imb4T3st4pp',
+}));
+
+server.use(express.static('public'));
+
+const proxy = createProxyMiddleware({
+  target: 'http://localhost:3000',
+  changeOrigin: true,
+});
+
 module.exports = function (eleventyConfig) {
-  // Options to customise the appearance of your design history
-  // https://x-govuk.github.io/govuk-eleventy-plugin/options/
-  eleventyConfig.addPlugin(require('@x-govuk/govuk-eleventy-plugin'), {
-    stylesheets: [
-      '/styles/application.css'
-    ],
-    headingPermalinks: true,
-    header: {
-      organisationLogo: false,
-      productName: 'Design history',
-      search: {
-        indexPath: '/search.json',
-        sitemapPath: '/sitemap'
-      }
-    }
-  })
+  eleventyConfig.setBrowserSyncConfig({
+    server: {
+      baseDir: 'public',
+    },
+    middleware: [proxy],
+    port: 3001,
+  });
 
-  // Passthrough
-  eleventyConfig.addPassthroughCopy({ './app/images': '.' })
-
-  // Config
   return {
     dataTemplateEngine: 'njk',
     htmlTemplateEngine: 'njk',
@@ -30,5 +37,9 @@ module.exports = function (eleventyConfig) {
       layouts: '_layouts',
       includes: '_components'
     }
-  }
-}
+  };
+};
+
+server.listen(3000, () => {
+  console.log('Express server is running on port 3000');
+});
